76292c7e013b960a529c5bdc95e15e6d4eb1121e
Akash <89548214+akash98k@users.noreply.github.com>
2022-09-20 01:58:00 +0530

    Update multiclass.py

--- objectDetection/multiclass.py
+++ objectDetection/multiclass.py
@@ -1,72 +1,57 @@
 import cv2
-import numpy as np 
-import argparse
-import time
+import numpy as np
 
-def load_yolo():
-	net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-	classes = []
-	with open("coco.names", "r") as f:
-		classes = [line.strip() for line in f.readlines()]
-	layers_names = net.getLayerNames()
-	output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
-	colors = np.random.uniform(0, 255, size=(len(classes), 3))
-	return net, classes, colors, output_layers
+net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+classes = []
+with open('coco.names', 'r') as f:
+        classes =f.read().splitlines()
 
-def load_image(image):
-    	# image loading
-	img = cv2.imread(image.jpg)
-	img = cv2.resize(img, None, fx=0.4, fy=0.4)
-	height, width, channels = img.shape
-	return img, height, width, channels
+img = cv2.imread('im.jpg')
+height, width, _ = img.shape
 
-def detect_objects(img, net, outputLayers):			
-	blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(416, 416), mean=(0, 0, 0), swapRB=True, crop=False)
-	net.setInput(blob)
-	outputs = net.forward(outputLayers)
-	return blob, outputs
+blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
 
-def get_box_dimensions(outputs, height, width):
-    boxes = []
-    confs = []
-    class_ids = []
-    for output in outputs:
-		    for detect in output:
-			    scores = detect[5:]
-			    print(scores)
-			    class_id = np.argmax(scores)
-			    conf = scores[class_id]
-			    if conf > 0.3:
-					    center_x = int(detect[0] * width)
-					    center_y = int(detect[1] * height)
-					    w = int(detect[2] * width)
-					    h = int(detect[3] * height)
-					    x = int(center_x - w/2)
-					    y = int(center_y - h / 2)
-					    boxes.append([x, y, w, h])
-					    confs.append(float(conf))
-					    class_ids.append(class_id)
-    return boxes, confs, class_ids
+net.setInput(blob)
 
-def draw_labels(boxes, confs, colors, class_ids, classes, img): 
-	indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
-	font = cv2.FONT_HERSHEY_PLAIN
-	for i in range(len(boxes)):
-		if i in indexes:
-			x, y, w, h = boxes[i]
-			label = str(classes[class_ids[i]])
-			color = colors[i]
-			cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
-			cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
-	cv2.imshow("Image", img)
- 
-	def image_detect(image): 
-		model, classes, colors, output_layers = load_yolo()
-		image, height, width, channels = load_image(image.jpg)
-		blob, outputs = detect_objects(image, model, output_layers)
-		boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
-		draw_labels(boxes, confs, colors, class_ids, classes, image)
-		while True:
-			key = cv2.waitKey(1)
-			if key == 27:
-				break
+output_layers_names = net.getUnconnectedOutLayersNames()
+layerOutputs = net.forward(output_layers_names)
+
+boxes =[]
+confidences = []
+class_ids = []
+
+for output in layerOutputs:
+    for detection in output:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            center_x = int(detection[0]*width)
+            center_y = int(detection[1]*height)
+            w = int(detection[2]*width)
+            h = int(detection[3]*height)
+
+            x = int(center_x - w/2)
+            y = int(center_y - h/2)
+
+            boxes.append([x, y, w, h])
+            confidences.append((float(confidence)))
+            class_ids.append(class_id)
+
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+font = cv2.FONT_HERSHEY_PLAIN
+colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+
+for i in indexes.flatten():
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confidence = str(round(confidences[i], 2))
+        color = colors[i]
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(img, label + " " + confidence, (x, y + 20), font, 2, (55, 255, 55), 2)
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
